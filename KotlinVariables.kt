1605a099e4cf03988ef4aaa57cc3a89d4e41008f
Bullseye198 <57558418+Bullseye198@users.noreply.github.com>
2020-01-23 22:56:34 +0100

    Add files via upload
    Basics of Kotlin variables.

---
+++ KotlinVariables.kt
@@ -0,0 +1,18 @@
+class KotlinVariables {
+	
+	var number = 42 //This is an Int type.
+	var message = "Hello" //This is a String.
+	
+	fun main(){
+		
+	//Usages of the variable must use only the name
+	
+	number = 10
+	number += 7
+	println(number)
+	println(message + "there")
+		
+	}
+		
+	//var is a mutable variable, val is assign-once variable.
+}
