54b62f3cf66a6f2d59d5686fed01b456ec7c9bd2
Bullseye198 <57558418+Bullseye198@users.noreply.github.com>
2020-01-24 23:03:03 +0100

    Update ValAndVarVariables.kt

--- ValAndVarVariables.kt
+++ ValAndVarVariables.kt
@@ -1,11 +1,13 @@
-//written by Bullseye198
+//Written by Bullseye198
 
 //You can declare the variable outside of enclosing function. For example here, and it works:
 //val name:String = "Nate"
 
+
 fun main(){
     //there are 2 types of variables in kotlin, mutable (Var) and local read only variables (val) they are used only once.
     
+    //Kotlin will automatically recognize the value, so no need to type in : String
     val name: String = "Nate"
     
     //name = "" //This gives us an error because val cannot be reassigned. We must use var.
