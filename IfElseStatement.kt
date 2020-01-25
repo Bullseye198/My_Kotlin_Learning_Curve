e074801a73794e4a61256d206db8d8366597b5c5
Bullseye198 <57558418+Bullseye198@users.noreply.github.com>
2020-01-25 08:36:28 +0100

    Create IfElseStatement.kt

---
+++ IfElseStatement.kt
@@ -0,0 +1,19 @@
+//Written by Bullseye198
+
+val name = "Nate"
+var greeting = "Mark"
+
+fun main(){
+   
+   //We have if and else statements similar like in Java.
+   
+      if(greeting != null){
+        println(greeting)
+    } else {
+        println ("Hi")
+    }
+
+    println(name)
+}
+
+//When we run it, it will print out "Mark" and "Nate" in our terminal.
