53eaa08b845db9d693fcc5024c05498879d46f3d
Bullseye198 <57558418+Bullseye198@users.noreply.github.com>
2020-01-25 14:02:09 +0100

    Create KotlinArraysAndForLoops.kt
    How to type array and list out value inside of an array.

---
+++ KotlinArraysAndForLoops.kt
@@ -0,0 +1,18 @@
+//Written by Bullseye198
+//Kotlin Arrays and For Loops and ForEach Loops
+
+fun main(){
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+    println(interestingThings.size)
+    println(interestingThings[0])  //Prints out first value inside of an array.
+
+    //With for loop we can print out each element in the array:
+    //first we define a variable. We called the variable interestingThing:
+    for (interestingThing in interestingThings) {
+        println(interestingThing)
+    }
+    //we can do the same thing with forEach loop:
+    interestingThings.forEach {
+        println(it) //it is the default name for each element in the array.
+    }
+}
